27f11ec4f71c83802199922377527df3a08028d9
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 08:03:47 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class cacheSetchWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = cacheSetchWorker()
+    print("Worker engine initialized.")
